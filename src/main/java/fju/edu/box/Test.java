6c749c9d4797c2813f355a493cf11c4283c8b531
Junpin <tyuighkj86@gmail.com>
2021-04-27 15:44:17 +0800

    third box

--- src/main/java/fju/edu/box/Test.java
+++ src/main/java/fju/edu/box/Test.java
@@ -1,11 +1,17 @@
 package fju.edu.box;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
         Box[] boxes = {new Box1(),new Box2(),new Box3(),new Box4(),new Box5(),new Box6() };
-        int len = 10;
-        int wid = 15;
-        int hig = 15;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please enter object's lenth");
+        int len = Integer.parseInt(scan.next());
+        System.out.println("please enter object's width");
+        int wid = Integer.parseInt(scan.next());
+        System.out.println("please enter object's width");
+        int hig = Integer.parseInt(scan.next());
         for (Box box : boxes){
             if(box.value(len ,wid ,hig )){
                 System.out.println(box.name + " it is, " + box.price);
