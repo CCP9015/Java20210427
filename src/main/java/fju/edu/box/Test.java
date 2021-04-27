a6d3146346602bca02e7c1a44dcb8b7624419a8f
Junpin <tyuighkj86@gmail.com>
2021-04-27 16:16:10 +0800

    fourth box

--- src/main/java/fju/edu/box/Test.java
+++ src/main/java/fju/edu/box/Test.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Box[] boxes = {new Box1(),new Box2(),new Box3(),new Box4(),new Box5(),new Box6() };
+       // Box[] boxes = {new Box1(),new Box2(),new Box3(),new Box4(),new Box5(),new Box6() };
+        Box[] boxes = new Box[6];
+        boxes[0] = new Box1();
+        boxes[1] = new Box2();
+        boxes[2] = new Box3();
+        boxes[3] = new Box4();
+        boxes[4] = new Box5();
+        boxes[0] = new Box6();
+        System.out.println(boxes[0].price);
         Scanner scan = new Scanner(System.in);
         System.out.println("please enter object's lenth");
         int len = Integer.parseInt(scan.next());
@@ -13,6 +21,7 @@ public class Test {
         System.out.println("please enter object's width");
         int hig = Integer.parseInt(scan.next());
         for (Box box : boxes){
+  //          Box1 box1 = (Box1) box;
             if(box.value(len ,wid ,hig )){
                 System.out.println(box.name + " it is, " + box.price);
                 break;
